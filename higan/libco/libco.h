62e39877da4bba23356cb65bc7c6dc6f0925bdc3
C.W. Betts <computers57@hotmail.com>
2021-02-22 14:32:56 -0700

    Update libco.

--- higan/libco/libco.h
+++ higan/libco/libco.h
@@ -1,20 +1,10 @@
 /*
-  libco
-  version: 0.17 (2015-06-18)
-  author: byuu
-  license: public domain
+  libco v20 (2019-10-16)
+  license: ISC
 */
 
 #ifndef LIBCO_H
 #define LIBCO_H
-
-#ifdef LIBCO_C
-  #ifdef LIBCO_MP
-    #define thread_local __thread
-  #else
-    #define thread_local
-  #endif
-#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -22,10 +12,12 @@ extern "C" {
 
 typedef void* cothread_t;
 
-cothread_t co_active();
+cothread_t co_active(void);
+cothread_t co_derive(void*, unsigned int, void (*)(void));
 cothread_t co_create(unsigned int, void (*)(void));
 void co_delete(cothread_t);
 void co_switch(cothread_t);
+int co_serializable(void);
 
 #ifdef __cplusplus
 }
